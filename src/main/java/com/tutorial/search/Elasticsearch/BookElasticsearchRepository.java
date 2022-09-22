a48d6caf8dadcb1c746a2001aa7a37b32b8c0888
Dmytro Werner <wernerdmytro@gmail.com>
2022-09-22 22:51:12 +0200

    removed no needed methods

--- src/main/java/com/tutorial/search/Elasticsearch/BookElasticsearchRepository.java
+++ src/main/java/com/tutorial/search/Elasticsearch/BookElasticsearchRepository.java
@@ -1,12 +1,7 @@
 package com.tutorial.search.Elasticsearch;
 
-import java.util.List;
-
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 import com.tutorial.search.Model.Book;
 
-public interface BookElasticsearchRepository extends ElasticsearchRepository<Book, String> {
-    List<Book> findByName(String name);
-    List<Book> findByIsbn(String isbn);
-}
+public interface BookElasticsearchRepository extends ElasticsearchRepository<Book, String> {}
